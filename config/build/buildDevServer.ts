c84348c64e02902937453218048940486ae74f0b
aleksandr_zelenskiy <dezion777@gmail.com>
2023-01-21 22:52:46 +0400

    BP-2 created webpack web server, send ENV and build

---
+++ config/build/buildDevServer.ts
@@ -0,0 +1,10 @@
+import { IBuildOptions } from "./types/configTypes";
+import { Configuration as DevServerConfiguration } from 'webpack-dev-server';
+
+export function buildDevServer(options: IBuildOptions): DevServerConfiguration {
+
+  return {
+    port: options.port,
+    open: true,
+  }
+}
--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,7 +1,7 @@
 import path from "path";
 import HTMLWebpackPlugin from 'html-webpack-plugin';
 import webpack from 'webpack';
-import { IBuildOptions } from "./types/config";
+import { IBuildOptions } from "./types/configTypes";
 
 export function buildPlugins({paths} : IBuildOptions): webpack.WebpackPluginInstance[] {
 
