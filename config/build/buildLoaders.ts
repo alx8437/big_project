c85a87535d303d115e9724b48f88ed04cf81e65f
aleksandr_zelenskiy <dezion777@gmail.com>
2023-01-25 16:34:58 +0400

    BP-4 Added rules for MiniCssLoader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,14 +1,15 @@
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import webpack from 'webpack'
+import { IBuildOptions } from './types/configTypes';
 
-export function buildLoaders(): webpack.RuleSetRule[] {
+export function buildLoaders(options: IBuildOptions): webpack.RuleSetRule[] {
   // Order loaders is important
 
   const cssLoader = {
       test: /\.s[ac]ss$/i,
       use: [
         // It creates a CSS file per JS file which contains CSS
-        MiniCssExtractPlugin.loader,
+        options.isDev ? 'style-loader': MiniCssExtractPlugin.loader,
         // Translates CSS into CommonJS
         "css-loader",
         // Compiles Sass to CSS
