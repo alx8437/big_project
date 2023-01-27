580c59993b6dab1ab1c62789711825bc412d52c5
aleksandr_zelenskiy <dezion777@gmail.com>
2023-01-27 17:36:13 +0400

    BP-6 bundle was devide on chanks by React.lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
 import {Link, Route, Routes} from 'react-router-dom';
+import {AboutPageAsync} from './pages/AboutPage/AboutPage.async';
+import {MainPageAsync} from './pages/MainPage/MainPage.async';
+import { Suspense } from 'react';
 import './index.scss';
-import AboutPage from './pages/AboutPage/AboutPage';
-import MainPage from './pages/MainPage/MainPage';
 
 const App = () => {
   return (
@@ -10,10 +10,12 @@ const App = () => {
       <Link to='/'>Main</Link>
       <Link to='/about'>About</Link>
 
-      <Routes>
-        <Route path='/about' element={<AboutPage />}/>
-        <Route path='/' element={<MainPage/>}/>
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path='/about' element={<AboutPageAsync />}/>
+          <Route path='/' element={<MainPageAsync/>}/>
+        </Routes>
+      </Suspense>
     </div>
   );
 };
