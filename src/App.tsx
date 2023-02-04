b6e88ff39c1cb5f29ecfc78e2d8626bf72c8c72f
aleksandr_zelenskiy <dezion777@gmail.com>
2023-02-04 21:53:59 +0400

    BP-7 add toggle themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import {Link, Route, Routes} from 'react-router-dom';
 import {AboutPageAsync} from './pages/AboutPage/AboutPage.async';
 import {MainPageAsync} from './pages/MainPage/MainPage.async';
-import { Suspense } from 'react';
+import { Suspense, useState } from 'react';
 import './styles/index.scss';
 
+export enum Theme {
+  LIGHT = 'light',
+  DARK = 'dark',
+}
+
 const App = () => {
+  const [theme, setTheme] = useState<Theme>(Theme.LIGHT)
+
+  const toggleTheme = () => {
+    setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK);
+  }
+
   return (
-    <div className='app'>
+    <div className={`app ${theme}`}>
+      <div>
+        <button onClick={toggleTheme}>Theme</button>
+      </div>
       <Link to='/'>Main</Link>
       <Link to='/about'>About</Link>
 
